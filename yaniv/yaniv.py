a8b875b94c1e517c1848a6d825d0f722a38c128a
elzurdo <eyalkazin@gmail.com>
2019-01-04 21:37:10 +0000

    refactoring and documentation

--- yaniv/yaniv.py
+++ yaniv/yaniv.py
@@ -26,6 +26,11 @@ def card_2_face(card):
     return card[1:]
 
 def deck(jokers=True):
+    '''Return the deck in dict type
+
+    :param jokers: bool. True: game played with 2 jokers, False: without
+    :return: dict. keys are the sting of the card and the values are the card points
+    '''
     suits = ['d', 'h', 'c', 's']  # diamonds, hearts, clubs, spades
 
     values = list(range(1, 11)) + [10, 10, 10]
@@ -93,23 +98,35 @@ class Player():
 
 class Game():
     def __init__(self, player_names, max_score=200, assaf_penalty=30, jokers=True, verbose=1, seed=4):
+        '''
+
+        :param player_names: list of names
+        :param max_score: int. Score which terminates game for player
+        :param assaf_penalty: int. The penalty score for an Assaf
+        :param jokers: bool. True to use 2 jokers in the game False without.
+        :param verbose: int
+        :param seed: int
+        '''
         self.seed = seed
-        self._players(player_names)
         self.assaf_penalty = assaf_penalty
         self.jokers = jokers
-        self.card2score = deck(jokers=jokers)
-
-        #self.all_players = players
         self.max_score = max_score
-
         self.verbose = verbose
 
+        self._players(player_names)
+        self.card2score = deck(jokers=jokers)
+
     def play(self):
-        self.initial_scores()
+        self.initiate_players_status()
 
         self.play_game()
 
     def _players(self, player_names):
+        '''Given a list of players names creates a list of of Player objects
+
+        :param player_names:
+        :return:
+        '''
 
         self.all_players = []
         print('Players:')
@@ -118,7 +135,14 @@ class Game():
             self.all_players.append(Player(name))
             print(name)
 
-    def initial_scores(self):
+    def initiate_players_status(self):
+        '''Initiates the status of all the players
+        In particular:
+        * score - int. starts at 0 points
+        * in_play - bool. True: plays current round. False: already lossed the game
+
+        :return: None
+        '''
         for iplayer, player in enumerate(self.all_players):
             player.score = 0
             player.in_play = True
@@ -145,25 +169,31 @@ class Game():
         return players
 
     def play_game(self):
-        card_num_2_max_value = self.card_number_2_max_card_value_to_declare_yaniv()
+        '''Game play
 
-        round_number = 0
+        :return:
+        '''
+
+        card_num_2_max_value = self.card_number_to_max_card_value_to_declare_yaniv()
+
+        round_number = 0 # first round number is 0
 
         players = self._round_players()
 
-        while len(players) > 1:
+        while len(players) > 1: # the game terminates when left with one player
             if self.verbose:
                 print('=' * 20)
             round_number += 1
             if self.verbose >= 1:
                 print('Round: {:,}'.format(round_number))
 
+            # Declaring Round object and playing a round
             self.round = Round(players, self.card2score, assaf_penalty=self.assaf_penalty, card_num_2_max_value=card_num_2_max_value, verbose=self.verbose, seed=self.seed)
             self.round.play()
 
             if self.seed:
                 self.seed += 1
-            # ====== score updating ==============
+            # ====== player score updating ==============
             for name, player in players.items():
                 player.score += player.hand_points
 
@@ -191,26 +221,51 @@ class Game():
         else:
             print("Everybody loses ... ({} players left)".format(len(players)))
 
-    def card_number_2_max_card_value_to_declare_yaniv(self):
-        # Evaluating the most extereme values to obtain a Yaniv
+    # TODO: build on this principle to make card_num_to_max_single_value in a dyanmic fashion, i.e, with knowldege of cards thrown out
+    def card_number_to_max_card_value_to_declare_yaniv(self):
+        '''Returns a mapping between the number of cards in the hand to the max possible single card value f
+        or successful Yaniv
+
+        E.g, if no jokers are in play, and someone has 5 cards, in order to successfully declare Yaniv,
+        the maximum value any single card may have is 3:
+        3, ace, ace, ace, ace
+
+        If there are jokers still in play, and someone has 5 cards, in order to in order to successfully declare Yaniv,
+        the maximum value any single card may have is 5:
+        5, joker, joker, ace, ace
+        4 is aso an option:
+        4, joker, joker, ace, 2 (or ace),
+        as is 3:
+        3, joker, joker, 2, 2 (or 3 and ace)
+
+        but 5 is the ultimate max.
+
+        This is useful to determine heuristics for probabilities.
+
+        Here we assume a full deck (i.e, no knowledge of what might have been thrown out).
+
+        :return: dict. keys are int of number of cards in hand. values are int of maximum possible value single card in
+        order to declare a successful Yaniv
+        '''
+        # Evaluating the most extreme values to obtain a Yaniv
         # No assumption about state of deck made (assuming a full deck)
 
         # making a function for future purposes
-        cardNum2maxValue = {}
-        cardNum2maxValue[
+        card_num_to_max_single_value = {}
+        card_num_to_max_single_value[
             5] = 3  # this means: if player has 5 cards in hand, the max value card for yaniv is 3: 3, ace, ace, ace, ace
-        cardNum2maxValue[4] = 4  # 4, ace, ace, ace
-        cardNum2maxValue[3] = 5  # 5, ace, ace
-        cardNum2maxValue[2] = 6  # 6, ace
-        cardNum2maxValue[1] = 7  # this means: if player has 1 card in hand, the max value card for yaniv is 7: 7
+        card_num_to_max_single_value[4] = 4  # 4, ace, ace, ace
+        card_num_to_max_single_value[3] = 5  # 5, ace, ace
+        card_num_to_max_single_value[2] = 6  # 6, ace
+        card_num_to_max_single_value[1] = 7  # this means: if player has 1 card in hand, the max value card for yaniv is 7: 7
 
         if self.jokers:
-            cardNum2maxValue[5] = 5  # 5, joker, joker, ace, ace
-            cardNum2maxValue[4] = 6  # 6, joker, joker, ace
-            cardNum2maxValue[3] = 7  # 7, joker, joker
-            cardNum2maxValue[2] = 7  # 7, joker
+            card_num_to_max_single_value[5] = 5  # 5, joker, joker, ace, ace
+            card_num_to_max_single_value[4] = 6  # 6, joker, joker, ace
+            card_num_to_max_single_value[3] = 7  # 7, joker, joker
+            card_num_to_max_single_value[2] = 7  # 7, joker
 
-        return cardNum2maxValue
+        return card_num_to_max_single_value
 
 
 class Round():
@@ -225,7 +280,6 @@ class Round():
         self.players = players
 
     def play(self):
-
         self.round_deck = dict(self.card2score)
 
         # print(len(self.round_deck))
