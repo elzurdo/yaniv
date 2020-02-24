82b6330d2323314055d52a1912a1c44527d7fab4
elzurdo <eyalkazin@gmail.com>
2020-02-24 08:05:18 +0000

    display card function and starting to implement mouse detection

--- yaniv/pygame_practice.py
+++ yaniv/pygame_practice.py
@@ -15,35 +15,83 @@ License: GPL, see http://www.gnu.org/licenses/gpl.html
 """
 
 # the next line is only needed for python2.x and not necessary for python3.x
-from __future__ import print_function, division
+#from __future__ import print_function, division
 import pygame
+
+CARD_WIDTH, CARD_HEIGHT = 222, 323
+MAX_CARDS = 5
+
+def display_card(card, screen, location=(100, 100), scale_frac=1., hidden=False):
+    card_file = '/Users/ekazin/Work/data_science/projects/playing-cards-assets/png/8_of_diamonds.png'
+    card_picture = pygame.image.load(card_file)
+    card_picture = card_picture.convert_alpha()
+
+    #111 / 162
+    scale_original = card_picture.get_size()
+    scale_ = (int(scale_original[0] * scale_frac), int(scale_original[1] * scale_frac))
+    card_picture = pygame.transform.scale(card_picture, scale_)
+
+    card_background = card_picture.copy()
+    if hidden:
+        card_background.fill((128, 0, 128))
+        screen.blit(card_background, location)
+    else:
+        card_background.fill((255, 255, 255))
+        screen.blit(card_background, location)
+        screen.blit(card_picture, location)
+
+
+size_factor = 2.
+screen_size = ( int(640 * size_factor), int(480 * size_factor))
 
 # Initialize Pygame.
 pygame.init()
 # Set size of pygame window.
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode(screen_size)
 # Create empty pygame surface.
 
+# ---- BackGround ----
 screen_size = screen.get_size()
-factor_ = 1. #0.5
+print(screen_size)
+factor_ = 1.  # 0.5
 background = pygame.Surface((factor_ * screen_size[0], factor_* screen_size[1]))
 # Fill the background white color.
-
-background.fill((255, 255, 255))
+poker_green = (75, 95, 59)
+background.fill(poker_green) # ((0, 255, 255))
 # Convert Surface object to make blitting faster.
 background = background.convert()
 # Copy background to screen (position (0, 0) is upper left corner).
 #screen.blit(background, (factor_/2. * screen_size[0], factor_/2. * screen_size[1]))
 screen.blit(background, (0,0))
 
-# create a rectangular surface for the ball
-ballsurface = pygame.Surface((50,50))
-# draw blue filled circle on ball surface
-pygame.draw.circle(ballsurface, (0,0,255), (25,25),27)
-screen.blit(ballsurface, (50,50))
+scale_frac = 0.7
+card_width = CARD_WIDTH  * scale_frac
+# ======= POV Cards =========
+bottom = screen_size[1] * 0.75
 
-card_file = '/Users/ekazin/Work/data_science/projects/playing-cards-assets/png/8_of_diamonds.png'
-mypicture = pygame.image.load(card_file)
+cards = ['dummy'] * 5
+
+ncards = len(cards)
+left_most = card_width / 2 + (MAX_CARDS - ncards) * CARD_WIDTH / 2
+
+
+for idx, card in enumerate(cards):
+    x_location = left_most + (card_width * 1.02) * idx
+    display_card(card, screen, location=(x_location, bottom), scale_frac=scale_frac, hidden=False)
+
+# ======= Opponent's Cards ========
+bottom = screen_size[1] * 0.05
+
+cards = ['dummy'] * 5
+
+ncards = len(cards)
+left_most = card_width / 2 + (MAX_CARDS - ncards) * CARD_WIDTH / 2
+
+
+for idx, card in enumerate(cards):
+    x_location = left_most + (card_width * 1.02) * idx
+    display_card(card, screen, location=(x_location, bottom), scale_frac=scale_frac, hidden=True)
+
 
 # Create Pygame clock object.
 clock = pygame.time.Clock()
@@ -53,6 +101,8 @@ mainloop = True
 FPS = 30
 # How many seconds the "game" is played.
 playtime = 0.0
+
+mouse = [(0,0), 0, 0, 0, 0, 0, 0] #(pos, b1,b2,b3,b4,b5,b6)
 
 while mainloop:
     # Do not go faster than this framerate.
@@ -69,7 +119,14 @@ while mainloop:
                 mainloop = False
             elif event.key == pygame.K_z:
                 print('I love Zurda!')
-
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse[event.button] = 1
+            mouse[0] = event.pos
+        elif event.type == pygame.MOUSEBUTTONUP:
+            mouse[event.button] = 0
+            mouse[0] = event.pos
+        elif event.type == pygame.MOUSEMOTION:
+            mouse[0] = event.pos
 
     # Print framerate and playtime in titlebar.
     text = "FPS: {0:.2f}   Playtime: {1:.2f}".format(clock.get_fps(), playtime)
